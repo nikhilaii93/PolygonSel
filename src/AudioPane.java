4564eb930bdcfe4b2a3a9e7dfd3977fafa1a5bdc
nikhilaii93 <nikhilverma.ajm@gmail.com>
2016-06-26 11:10:25 +0530

    Play button added in AudioPane

---
+++ src/AudioPane.java
@@ -0,0 +1,211 @@
+import java.awt.Dimension;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+
+import javax.sound.sampled.AudioFileFormat;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.SourceDataLine;
+import javax.sound.sampled.TargetDataLine;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JToggleButton;
+
+public class AudioPane extends JPanel {
+	/**
+	 * Auto-generated serialID
+	 */
+	private static final long serialVersionUID = -6483413280444018893L;
+
+	private JToggleButton recordButton;
+	private JButton playButton;
+	private TargetDataLine line;
+	protected boolean stopRecordingCalled = false;
+
+	SourceDataLine sourceDataLine;
+	AudioInputStream audioInputStream;
+	boolean stopPlayback = false;
+	protected String audioFileName = null;
+	
+	public AudioPane() {
+		setLayout(new GridBagLayout());
+		recordButton = new JToggleButton("Record");
+		recordButton.setEnabled(false);
+		recordButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (recordButton.isSelected()) {
+					startRecording();
+					recordButton.setText("Stop");
+				} else {
+					stopRecording();
+					playButton.setEnabled(true);
+					recordButton.setText("Record");
+				}
+			}
+		});
+		add(recordButton);
+		
+		playButton = new JButton("Play");
+		playButton.setEnabled(false);
+		playButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				playAudio(audioFileName);
+			}
+		});
+		add(playButton);
+	}
+
+	@Override
+	public Dimension getPreferredSize() {
+		return new Dimension(200, 200);
+	}
+
+	protected void stopRecording() {
+		stopRecordingCalled = true;
+		if (line != null) {
+			line.stop();
+			line.close();
+			line = null;
+		}
+	}
+
+	protected void startRecording() {
+		if (line == null) {
+			Thread t = new Thread(new Runnable() {
+
+				@Override
+				public void run() {
+					try {
+						stopRecordingCalled = false;
+						ContextDialogBox.format = getAudioFormat();
+						DataLine.Info info = new DataLine.Info(TargetDataLine.class, ContextDialogBox.format);
+
+						// checks if system supports the data line
+						if (!AudioSystem.isLineSupported(info)) {
+							System.out.println("Line not supported");
+							System.exit(0);
+						}
+						line = (TargetDataLine) AudioSystem.getLine(info);
+						line.open(ContextDialogBox.format);
+						line.start(); // start capturing
+
+						System.out.println("In utils.Recorder: Start capturing...");
+
+						ContextDialogBox.ais = new AudioInputStream(line);
+						if (audioFileName == null) {
+							audioFileName = "$AUDIO$" + ContextDialogBox.audioCounter + ".wav"; 
+						}
+						File f = new File(audioFileName);
+						ContextDialogBox.absPathTempFiles = f.getAbsoluteFile().getParent();
+
+						System.out.println("tempPath : " + ContextDialogBox.absPathTempFiles);
+
+						AudioSystem.write(ContextDialogBox.ais, AudioFileFormat.Type.WAVE, f);
+						/*
+						 * if (finalStream == null) { finalStream =
+						 * copyAudioInputStream(ais); } else { toAppendStream =
+						 * copyAudioInputStream(ais); }
+						 */
+					} catch (LineUnavailableException ex) {
+						ex.printStackTrace();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					System.out.println("Recording is done");
+				}
+			});
+			t.start();
+		}
+	}
+
+	protected AudioFormat getAudioFormat() {
+		float sampleRate = 16000;
+		int sampleSizeInBits = 8;
+		int channels = 2;
+		boolean signed = true;
+		boolean bigEndian = true;
+		AudioFormat format = new AudioFormat(sampleRate, sampleSizeInBits, channels, signed, bigEndian);
+		return format;
+	}
+
+	// TODO Append doesn't work for wav format getFrameLength() returns -1
+	/*
+	 * protected void appendRecordings() throws IOException { if (finalStream ==
+	 * null && toAppendStream != null) { finalStream =
+	 * copyAudioInputStream(toAppendStream); toAppendStream = null; } else if
+	 * (toAppendStream != null) { AudioInputStream appendedStreams = new
+	 * AudioInputStream( new SequenceInputStream(finalStream, toAppendStream),
+	 * finalStream.getFormat(), finalStream.getFrameLength() +
+	 * toAppendStream.getFrameLength()); finalStream =
+	 * copyAudioInputStream(appendedStreams); toAppendStream = null; } }
+	 */
+
+	private void playAudio(String fileName) {
+		try {
+			File soundFile = new File(fileName);
+			audioInputStream = AudioSystem.getAudioInputStream(soundFile);
+			DataLine.Info dataLineInfo = new DataLine.Info(SourceDataLine.class, ContextDialogBox.format);
+			sourceDataLine = (SourceDataLine) AudioSystem.getLine(dataLineInfo);
+
+			// Create a thread to play back the data and
+			// start it running. It will run until the
+			// end of file, or the Stop button is
+			// clicked, whichever occurs first.
+			// Because of the data buffers involved,
+			// there will normally be a delay between
+			// the click on the Stop button and the
+			// actual termination of playback.
+			new PlayThread().start();
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(0);
+		} // end catch
+	}// end playAudio
+
+	// Inner class to play back the data from the
+	// audio file.
+	class PlayThread extends Thread {
+		byte tempBuffer[] = new byte[10000];
+
+		public void run() {
+			try {
+				sourceDataLine.open(ContextDialogBox.format);
+				sourceDataLine.start();
+
+				int cnt;
+				// Keep looping until the input read method
+				// returns -1 for empty stream or the
+				// user clicks the Stop button causing
+				// stopPlayback to switch from false to
+				// true.
+				while ((cnt = audioInputStream.read(tempBuffer, 0, tempBuffer.length)) != -1 && stopPlayback == false) {
+					if (cnt > 0) {
+						// Write data to the internal buffer of
+						// the data line where it will be
+						// delivered to the speaker.
+						sourceDataLine.write(tempBuffer, 0, cnt);
+					} // end if
+				} // end while
+					// Block and wait for internal buffer of the
+					// data line to empty.
+				sourceDataLine.drain();
+				sourceDataLine.close();
+
+				// Prepare to playback another file
+				stopPlayback = false;
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.exit(0);
+			} // end catch
+		}// end run
+	}// end inner class PlayThread
+}
--- src/ContextDialogBox.java
+++ src/ContextDialogBox.java
@@ -1,5 +1,3 @@
-import java.awt.Dimension;
-import java.awt.GridBagLayout;
 import java.awt.Point;
 import java.awt.TextArea;
 import java.awt.event.ActionEvent;
@@ -12,9 +10,6 @@ import javax.sound.sampled.AudioFileFormat;
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.DataLine;
-import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.TargetDataLine;
 import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.AbstractButton;
 import javax.swing.BoxLayout;
@@ -29,9 +24,10 @@ public class ContextDialogBox {
 
 	public static ArrayList<String> titles = new ArrayList<String>();
 	public static ArrayList<String> descText = new ArrayList<String>();
-	static // public static ArrayList<AudioInputStream> descAudio = new ArrayList<AudioInputStream>();
+	static // public static ArrayList<AudioInputStream> descAudio = new
+			// ArrayList<AudioInputStream>();
 	int audioCounter = 0;
-	
+
 	public static JToggleButton audioCumText;
 	protected static AudioInputStream ais;
 
@@ -106,12 +102,11 @@ public class ContextDialogBox {
 			boolean contextAdded = false;
 			if (result == JOptionPane.OK_OPTION) {
 				if (polygonIndex == -1) {
-					if (audioCumText.isSelected() && /* finalStream */ais != null
-							&& audioDesc.stopRecordingCalled) {
-						System.out.println("yaha1");
+					if (audioCumText.isSelected() && /* finalStream */ais != null && audioDesc.stopRecordingCalled) {
 						titles.add(title.getText());
 						descText.add("$AUDIO$" + audioCounter);
-						// AudioInputStream copyStream = copyAudioInputStream(/* finalStream */ais);
+						// AudioInputStream copyStream = copyAudioInputStream(/*
+						// finalStream */ais);
 						// descAudio.add(copyStream);
 						audioCounter++;
 						@SuppressWarnings("unchecked")
@@ -119,7 +114,6 @@ public class ContextDialogBox {
 						Utility.polygons.add(copyCoords);
 						contextAdded = true;
 					} else if (!audioCumText.isSelected()) {
-						System.out.println("yaha2");
 						titles.add(title.getText());
 						descText.add(textDesc.getText());
 						// descAudio.add(null);
@@ -131,15 +125,14 @@ public class ContextDialogBox {
 					}
 				} else {
 					if (audioCumText.isSelected() && /* finalStream */ais != null) {
-						System.out.println("yaha3");
 						titles.set(polygonIndex, title.getText());
 						descText.set(polygonIndex, "$AUDIO$" + polygonIndex);
-						// AudioInputStream copyStream = copyAudioInputStream(/* finalStream */ais);
+						// AudioInputStream copyStream = copyAudioInputStream(/*
+						// finalStream */ais);
 						// descAudio.add(copyStream);
 						audioCounter++;
 						contextAdded = true;
 					} else if (!audioCumText.isSelected()) {
-						System.out.println("yaha4");
 						titles.set(polygonIndex, title.getText());
 						descText.set(polygonIndex, textDesc.getText());
 						// descAudio.set(polygonIndex, null);
@@ -158,120 +151,6 @@ public class ContextDialogBox {
 		return false;
 	}
 
-	public static class AudioPane extends JPanel {
-		/**
-		 * Auto-generated serialID
-		 */
-		private static final long serialVersionUID = -6483413280444018893L;
-
-		private JToggleButton recordButton;
-		private TargetDataLine line;
-		protected boolean stopRecordingCalled = false;
-
-		public AudioPane() {
-			setLayout(new GridBagLayout());
-			recordButton = new JToggleButton("Record");
-			recordButton.setEnabled(false);
-			recordButton.addActionListener(new ActionListener() {
-				@Override
-				public void actionPerformed(ActionEvent e) {
-					if (recordButton.isSelected()) {
-						startRecording();
-						recordButton.setText("Stop");
-					} else {
-						stopRecording();
-						recordButton.setText("Record");
-					}
-				}
-			});
-			add(recordButton);
-		}
-
-		@Override
-		public Dimension getPreferredSize() {
-			return new Dimension(200, 200);
-		}
-
-		protected void stopRecording() {
-			stopRecordingCalled = true;
-			if (line != null) {
-				line.stop();
-				line.close();
-				line = null;
-			}
-		}
-
-		protected void startRecording() {
-			if (line == null) {
-				Thread t = new Thread(new Runnable() {
-
-					@Override
-					public void run() {
-						try {
-							stopRecordingCalled = false;
-							format = getAudioFormat();
-							DataLine.Info info = new DataLine.Info(TargetDataLine.class, format);
-
-							// checks if system supports the data line
-							if (!AudioSystem.isLineSupported(info)) {
-								System.out.println("Line not supported");
-								System.exit(0);
-							}
-							line = (TargetDataLine) AudioSystem.getLine(info);
-							line.open(format);
-							line.start(); // start capturing
-
-							System.out.println("In utils.Recorder: Start capturing...");
-
-							ais = new AudioInputStream(line);
-							File f = new File("$AUDIO$"+audioCounter+".wav");
-							absPathTempFiles = f.getAbsoluteFile().getParent();
-							
-							System.out.println("tempPath : " + absPathTempFiles);
-							
-							AudioSystem.write(ais, AudioFileFormat.Type.WAVE, f);
-							/*
-							 * if (finalStream == null) { finalStream =
-							 * copyAudioInputStream(ais); } else {
-							 * toAppendStream = copyAudioInputStream(ais); }
-							 */
-						} catch (LineUnavailableException ex) {
-							ex.printStackTrace();
-						} catch (IOException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
-						System.out.println("Recording is done");
-					}
-				});
-				t.start();
-			}
-		}
-
-		protected AudioFormat getAudioFormat() {
-			float sampleRate = 16000;
-			int sampleSizeInBits = 8;
-			int channels = 2;
-			boolean signed = true;
-			boolean bigEndian = true;
-			AudioFormat format = new AudioFormat(sampleRate, sampleSizeInBits, channels, signed, bigEndian);
-			return format;
-		}
-
-		// TODO Append doesn't work for wav format getFrameLength() returns -1
-		/*
-		 * protected void appendRecordings() throws IOException { if
-		 * (finalStream == null && toAppendStream != null) { finalStream =
-		 * copyAudioInputStream(toAppendStream); toAppendStream = null; } else
-		 * if (toAppendStream != null) { AudioInputStream appendedStreams = new
-		 * AudioInputStream( new SequenceInputStream(finalStream,
-		 * toAppendStream), finalStream.getFormat(),
-		 * finalStream.getFrameLength() + toAppendStream.getFrameLength());
-		 * finalStream = copyAudioInputStream(appendedStreams); toAppendStream =
-		 * null; } }
-		 */
-	}
-
 	protected static AudioInputStream copyAudioInputStream(AudioInputStream ais) throws IOException {
 		AudioSystem.write(ais, AudioFileFormat.Type.WAVE, new File("C:/Users/Nikhil/Documents/trial2.wav"));
 		File tempFile = File.createTempFile("wav", "tmp");
@@ -287,7 +166,7 @@ public class ContextDialogBox {
 		}
 		tempFile.delete();
 		AudioSystem.write(aisCopy, AudioFileFormat.Type.WAVE, new File("C:/Users/Nikhil/Documents/trial1.wav"));
-		
+
 		return aisCopy;
 	}
 
